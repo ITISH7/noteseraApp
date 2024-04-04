c3cff5e9096b3ac5db5f06c06352abbcc0d62fa4
unknown <itishjain7@gmail.com>
2024-04-04 16:48:59 +0530

    changes by dusht-insaan

--- notesera/screens/subjectScreen.js
+++ notesera/screens/subjectScreen.js
@@ -1,13 +1,31 @@
-import { Text, View } from "react-native"
+import { useNavigation, useRoute } from "@react-navigation/native"
+import { Text, View, TouchableOpacity } from "react-native"
 import { Image } from "react-native"
+import * as Icon from 'react-native-feather';
+import { themeColors } from "../themes";
 
-const SubjectScreen=()=>{
- return(<View>
-    <View className="relative">
-        <Image></Image>
-        
-    </View>
-        
+const SubjectScreen = () => {
+    const { params } = useRoute()
+    const subjectData = params
+    const navigate = useNavigation()
+
+    return (<View>
+        <View className="relative">
+            <Image className="w-full h-56" source={{ uri: subjectData.subjectImage }}></Image>
+            <TouchableOpacity onPress={() => {
+                navigate.goBack()
+            }}
+                className="absolute top-10 left-2 bg-gray-50 rounded-full shadow p-2">
+                <Icon.ArrowLeft strokeWidth={3} stroke={themeColors.bgColor(1)}></Icon.ArrowLeft>
+            </TouchableOpacity>
+        </View>
+        <View style={{ borderTopRightRadius: 40, borderTopLeftRadius: 40 }}
+                className="bg-white -mt-8 pt-0">
+                <View className="px-5">
+                    <Text className="text-2xl font-bold text-center mt-0 ">Units</Text>
+                </View>
+            </View>
+
     </View>)
 }
 export default SubjectScreen
